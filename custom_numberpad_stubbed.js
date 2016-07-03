c606bd80911018d451c69f5b2278510dfa6a25ee
Vincent Alcantara <vincent.alcantara@yahoo.com.au>
2016-07-03 16:25:35 +1000

    Styled the number pad to use table and added styling to the correct/incorrect status

--- custom_numberpad_stubbed.js
+++ custom_numberpad_stubbed.js
@@ -9,46 +9,67 @@ LearnosityAmd.define(['jquery-v1.10.2'], function ($) {
         
         //Build up the structure you want to use here, and add any event handling you may want to use
         
-        $htmlObj = '<script>function setText(id, value) {  document.getElementById(id).value = value;}</script>' +
-        '<div id="numberpad1" class="col-xs-4 btn-group-vertical" role="group" aria-label="...">' +
-        '<input id="column1" class="btn btn-secondary btn-block" disabled=disabled/>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 0)">0</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 1)">1</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 2)">2</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 3)">3</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 4)">4</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 5)">5</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 6)">6</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 7)">7</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 8)">8</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column1\', 9)">9</button>' +
-        '</div>' +
-        '<div id="numberpad2" class="col-xs-4 btn-group-vertical" role="group aria-label="...">' +
-        '<input id="column2" class="btn btn-secondary btn-block" disabled=disabled/>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 0)">0</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 1)">1</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 2)">2</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 3)">3</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 4)">4</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 5)">5</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 6)">6</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 7)">7</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 8)">8</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column2\', 9)">9</button>' +
-        '</div>' +
-        '<div id="numberpad3" class="col-xs-4 btn-group-vertical" role="group" aria-label="...">' +
-        '<input id="column3" class="btn btn-secondary btn-block" disabled=disabled/>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 0)">0</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 1)">1</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 2)">2</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 3)">3</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 4)">4</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 5)">5</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 6)">6</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 7)">7</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 8)">8</button>' +
-        '<button type="button" class="btn btn-default btn-block" onclick="setText(\'column3\', 9)">9</button>' +
-        '</div>';
+        $htmlObj = '<script>function setText(id, value) {' + 
+            'document.getElementById(id).value = value;' +
+            '}' + 
+        '</script>' +
+        '<table class="table">' +
+          '<tr id="responseRow" class="">' +
+            '<td><input value="" id="number1" class="table-header"/></td>' +
+            '<td><input value="" id="number2" class="table-header"/></td>' +
+            '<td><input value="" id="number3" class="table-header"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="0" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="0" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="0" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="1" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="1" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="1" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="2" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="2" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="2" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="3" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="3" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="3" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="4" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="4" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="4" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="5" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="5" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="5" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="6" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="6" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="6" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="7" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="7" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="7" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="8" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="8" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="8" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>' +
+          '<tr>' +
+            '<td><input type="button" class="btn-option" value="9" onclick="setText(\'number1\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="9" onclick="setText(\'number2\', value)"/></td>' +
+            '<td><input type="button" class="btn-option" value="9" onclick="setText(\'number3\', value)"/></td>' +
+          '</tr>'+
+        '</table>';
         
         return $htmlObj;
     }
@@ -61,9 +82,13 @@ LearnosityAmd.define(['jquery-v1.10.2'], function ($) {
         $('#message').remove();
 
         if (questionAnswerStatus) {
-            messageHTML = '<div  id="message" class="alert alert-success" role="alert">Correct</div>';
+            messageHTML = '<strong id="message">Correct</strong>'
+            $('#responseRow').removeClass('row-invalid');
+            $('#responseRow').addClass('row-valid');
         } else {
-            messageHTML = '<div  id="message" class="alert alert-danger" role="alert">Incorrect</div>';
+            messageHTML = '<strong id="message">Incorrect</strong>';
+            $('#responseRow').removeClass('row-valid');
+            $('#responseRow').addClass('row-invalid');
         }
         $(messageHTML).insertBefore('.lrn_question');
            
@@ -86,10 +111,10 @@ LearnosityAmd.define(['jquery-v1.10.2'], function ($) {
         //add on validate button.
         init.events.on('validate', function () {
             // Determine the response by adding the numbers for each column multiplied by its degree
-            // Note the by multiplying by a number the result becomes a number
-            var $response1 = $('#column1', init.$el).val() * 100;
-            var $response2 = $('#column2', init.$el).val() * 10;
-            var $response3 = $('#column3', init.$el).val() * 1;
+            // Note by multiplying by a number the result becomes a number
+            var $response1 = $('#number1', init.$el).val() * 100;
+            var $response2 = $('#number2', init.$el).val() * 10;
+            var $response3 = $('#number3', init.$el).val() * 1;
 
             init.response = $response1 + $response2 + $response3;
 
